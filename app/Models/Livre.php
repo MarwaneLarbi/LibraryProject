33211907a670c452945538c4b94ce3d34df7f7d8
Marwane <marwane.dz2@gmail.com>
2021-12-20 19:00:31 +0100

    update

--- app/Models/Livre.php
+++ app/Models/Livre.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Livre extends Model
 {
     use HasFactory;
+    use SoftDeletes;
+    public function Livre(){
+
+        return $this->belongsTo(User::class);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,9 @@ Route::get('/auteurs2', function () {
 Route::get('/Category&MotsCles', function () {
     return view('Category');
 });
+Route::get('/livre', function () {
+    return view('Livre');
+});
 Route::post('livewire',[\App\Http\Livewire\AddAuteurForm::class,'store'])->name('AddAuteurForm.store');
 Route::post('checkAuteur', [\App\Http\Livewire\AddAuteurForm::class,'Check'])->name('AddAuteurForm.check');
 Route::get('/getAuteur', [\App\Http\Livewire\ListeAuteurs::class,'getAuteurs'])->name('AddAuteurForm.Getauteurs');
@@ -32,8 +35,13 @@ Route::get('auteurs/list', [\App\Http\Livewire\ListeAuteurs::class, 'getAuteurs'
 Route::get('/auteurs/edit/{id}',[\App\Http\Livewire\EditerAuteur::class,'editer'])->name('AddAuteurForm.editer');
 Route::post('checkEditAuteur', [\App\Http\Livewire\EditerAuteur::class,'Check'])->name('EditerAuteur.check');
 Route::post('storeEditAuteur', [\App\Http\Livewire\EditerAuteur::class,'store'])->name('EditerAuteur.store');
-Route::get('/auteurs/delete/{id}', [\App\Http\Livewire\DeleteAuteurForm::class,'deleteAuteur'])->name('Auteur.delete');
+Route::get('/auteurs/description/{id}', [\App\Http\Livewire\ListeAuteurs::class,'getDescription'])->name('Auteur.description');
 Route::post('/category/checkadd', [\App\Http\Livewire\AddCategories::class,'checkAdd'])->name('category.store');
 Route::get('/category/editer/{id}', [\App\Http\Livewire\EditCategories::class,'editer'])->name('category.edit');
 Route::post('/category/editer/', [\App\Http\Livewire\EditCategories::class,'store'])->name('category.upadate');
-
+Route::get('/category/delete/{id}', [\App\Http\Livewire\DeleteCategories::class,'deleteCategory'])->name('category.delete');
+Route::get('/livre/gen', [\App\Http\Livewire\AddLivre::class,'generateBook'])->name('livre.gen');
+Route::get('/livre/add/check', [\App\Http\Livewire\AddLivre::class,'check'])->name('livre.add.check');
+Route::post('/livre/add/', [\App\Http\Livewire\AddLivre::class,'store'])->name('livre.store');
+Route::post('/storeTags', [\App\Http\Livewire\MotsCles::class,'store'])->name('Tags.store');
+Route::get('/tags/delete/{id}', [\App\Http\Livewire\MotsCles::class,'delete'])->name('tags.delete');
